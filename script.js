e7e126a22975c78482ef2917977aab6c73f8cf9b
Pedro Navarro <54799795+pedronavarrodev@users.noreply.github.com>
2022-01-19 18:02:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 10
+    msg.innerHTML = `Agora são ${hora} horas `
+
+    if (hora >= 0 && hora < 12){
+        //BOM DIA
+        img.scr = 'manha.png'
+        document.body.style.background = '#e0c66e'
+    }else if(hora >= 12 & hora < 18){
+        //BOA TARDE
+        img.src = 'tarde.png'
+        document.body.style.background = '#826548'
+    }else{
+        //BOA NOITE
+        img.src = 'noite.png'
+        document.body.style.background = '#303030'
+    }
+
+
+}
